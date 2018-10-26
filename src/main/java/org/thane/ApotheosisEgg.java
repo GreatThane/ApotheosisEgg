4601402577209624f42f5803a8327ccc5a980e93
GreatThane <ep9630@gmail.com>
2018-10-26 00:13:16 -0500

    Fix attempts

--- src/main/java/org/thane/ApotheosisEgg.java
+++ src/main/java/org/thane/ApotheosisEgg.java
@@ -1,19 +1,34 @@
 package org.thane;
 
 import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.red.Defend_Red;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.RelicLibrary;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 public class ApotheosisEgg extends CustomRelic {
 
     public static final String ID = "Apotheosis Egg";
+    public static final String IMG = "images/relics/apotheosisEgg.png";
+    public static final String OUTLINE = "images/relics/outline/apotheosisEgg.png";
+
+//    private Set<UUID> upgradedCards = new HashSet<>();
 
     public ApotheosisEgg() {
-        super(ID, new Texture("apotheosisEgg.png"), RelicTier.SPECIAL, LandingSound.MAGICAL);
+        super(ID,
+                new Texture(Gdx.files.internal(IMG)),
+                RelicTier.SPECIAL,
+                LandingSound.MAGICAL);
     }
 
     @Override
@@ -22,10 +37,24 @@ public class ApotheosisEgg extends CustomRelic {
     }
 
     @Override
+    public void onObtainCard(AbstractCard c) {
+        if (c.canUpgrade()) {
+            c.upgrade();
+            flash();
+//            upgradedCards.add(c.uuid);
+        }
+    }
+
+    @Override
     public void onUnequip() {
 //        AbstractDungeon.getCurrRoom().spawnRelicAndObtain(Settings.WIDTH / 2, Settings.HEIGHT / 2, RelicLibrary.getRelic("Molten Egg 2").makeCopy());
 //        AbstractDungeon.getCurrRoom().spawnRelicAndObtain(Settings.WIDTH / 2, Settings.HEIGHT / 2, RelicLibrary.getRelic("Toxic Egg 2").makeCopy());
 //        AbstractDungeon.getCurrRoom().spawnRelicAndObtain(Settings.WIDTH / 2, Settings.HEIGHT / 2, RelicLibrary.getRelic("Frozen Egg 2").makeCopy());
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0] + DESCRIPTIONS[1] + DESCRIPTIONS[2] + DESCRIPTIONS[3];
     }
 
     @Override
--- src/main/java/org/thane/ApotheosisEggMod.java
+++ src/main/java/org/thane/ApotheosisEggMod.java
@@ -3,16 +3,24 @@ package org.thane;
 import basemod.BaseMod;
 import basemod.helpers.RelicType;
 import basemod.interfaces.EditRelicsSubscriber;
+import basemod.interfaces.EditStringsSubscriber;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 
 import basemod.interfaces.RelicGetSubscriber;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.RelicLibrary;
+import com.megacrit.cardcrawl.localization.RelicStrings;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.charset.StandardCharsets;
 
 @SpireInitializer
-public class ApotheosisEggMod implements RelicGetSubscriber, EditRelicsSubscriber {
+public class ApotheosisEggMod implements RelicGetSubscriber, EditRelicsSubscriber, EditStringsSubscriber {
     public static void initialize() {
         new ApotheosisEggMod();
     }
@@ -42,6 +50,21 @@ public class ApotheosisEggMod implements RelicGetSubscriber, EditRelicsSubscribe
 
     @Override
     public void receiveEditRelics() {
-        BaseMod.addRelic(new ApotheosisEgg(), RelicType.SHARED);
+        Logger logger = LogManager.getLogger(ApotheosisEggMod.class.getName());
+        logger.info("HELLO THIS IS MY NAME NOTICE ME");
+        logger.info(ApotheosisEgg.ID == null);
+        logger.info(ApotheosisEgg.IMG == null);
+        logger.info(ApotheosisEgg.OUTLINE == null);
+        logger.info(new Texture(Gdx.files.internal(ApotheosisEgg.IMG)) == null);
+        logger.info(new Texture(Gdx.files.internal(ApotheosisEgg.OUTLINE)) == null);
+        ApotheosisEgg apotheosisEgg = new ApotheosisEgg();
+        logger.info(apotheosisEgg);
+        BaseMod.addRelic(apotheosisEgg, RelicType.SHARED);
+    }
+
+    @Override
+    public void receiveEditStrings() {
+        String relicStrings = Gdx.files.internal("localization/relics.json").readString(String.valueOf(StandardCharsets.UTF_8));
+        BaseMod.loadCustomStrings(RelicStrings.class, relicStrings);
     }
 }
